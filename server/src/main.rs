984a7f46d337e7fc7096a25b94e10564bbecf0fd
Leo Leite <leonardossleite@gmail.com>
2024-02-23 22:52:40 +0000

    autocommit

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,27 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{extract::Path, routing::get, Json, Router};
 use dotenv::dotenv;
+use serde::Serialize;
 use shared::get_pg_pool::get_pg_pool;
 use sqlx::{PgPool, Row};
 use std::env;
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+#[derive(Serialize)]
+struct Person {
+    id: i32,
+    name: String,
+    age: i32,
+    city: String,
+}
+
+async fn handler() -> Json<Person> {
+    let user = Person {
+        id: 1,
+        name: String::from("John"),
+        age: 30,
+        city: String::from("New York"),
+    };
+
+    Json(user)
 }
 
 #[tokio::main]
