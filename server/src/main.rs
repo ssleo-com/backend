1aab2d2f85627aa88ed47072d9b0131199c39fff
Leo Leite <leonardossleite@gmail.com>
2024-02-28 03:58:03 +0000

    autocommit

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,37 +1,31 @@
 use axum::{routing::get, Json, Router};
+use chrono::NaiveDate;
 use dotenv::dotenv;
-
-use models::person::{NewPerson, Person};
+use models::person::NewPerson;
+use models::person::Save;
 use shared::get_pg_pool::get_pg_pool;
 
-async fn handler() -> Json<Person> {
-    let user = Person {
-        id: 1,
-        first_name: String::from("John"),
+async fn handler() -> Json<NewPerson> {
+    let pool = get_pg_pool().await;
+
+    let from_ymd_opt = NaiveDate::from_ymd_opt;
+
+    let new_user = NewPerson {
+        first_name: String::from("Jane"),
         last_name: String::from("Doe"),
-        date_of_birth: Some(String::from("1990-01-01")),
-        created_at: String::from("2021-01-01"),
-        updated_at: String::from("2021-01-01"),
+        date_of_birth: from_ymd_opt(2015, 3, 14),
     };
 
-    Json(user)
+    new_user.save(&pool).await.unwrap();
+
+    Json(new_user)
 }
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let pool = get_pg_pool().await;
-
     let app = Router::new().route("/", get(handler));
-
-    let new_user = NewPerson {
-        first_name: String::from("Jane"),
-        last_name: String::from("Doe"),
-        date_of_birth: Some(String::from("1990-01-01")),
-    };
-
-    new_user.save(&pool).await.unwrap();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:9090").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
