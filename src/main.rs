5dbe6efa64f743258e12abd8437997fde47a2b85
Leo Leite <leonardossleite@gmail.com>
2024-03-30 02:02:27 +0000

    initial implementation for jwt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use axum::{extract::Path, Json};
 use axum::{routing::get, Router};
 mod controllers;
@@ -7,8 +9,43 @@ use controllers::{
 use dotenv::dotenv;
 mod models;
 mod shared;
+use serde::{Deserialize, Serialize};
 use shared::{get_env_var::get_env_var, get_pg_pool::get_pg_pool};
 use sqlx::PgPool;
+
+use jsonwebtoken::{encode, EncodingKey, Header};
+
+/// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    company: String,
+    exp: usize,
+    iat: i64,
+}
+
+fn issue_jwt(user_id: usize) -> Result<String, Box<dyn std::error::Error>> {
+    let now = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+    // Define your payload (claims) - for example, the user ID
+    let claims = Claims {
+        sub: user_id.to_string(), // Subject
+        company: user_id.to_string(),
+        exp: 10000000000, // Expiration time
+        iat: now as i64,
+    };
+
+    // Define your secret key
+    let secret = "your_secret_key";
+
+    // Encode the JWT using the HS256 algorithm and your secret key
+    let token = encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(secret.as_ref()),
+    )?;
+
+    Ok(token)
+}
 
 #[tokio::main]
 async fn main() {
@@ -27,6 +64,8 @@ async fn main() {
                 .patch(patch_handler)
                 .delete(delete_handler),
         );
+
+    println!("{}", issue_jwt(1).unwrap());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:9090").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ cargo build --release;
 sudo cp ./target/release/backend /usr/local/bin/backend;
 cd migrations;
 cargo build --release;
-sudo cp ./target/release/migrations /usr/local/bin/migrate;
+sudo cp ./target/release/migrations /usr/local/bin/migrate-rs;
 cd -;
